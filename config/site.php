ed54c0e10250992122f13e8f6a2f33fe9327d693
Nick Kornushin <develop@floristum.ru>
2020-03-08 10:18:51 +0300

    some-changes

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
         'phones' => [
                 
                 'hot' => '8 800 600-54-97',
-                'hot_normalized' => '+78129822383'
+                'hot_normalized' => '88006005497'
                /*
                 'hot' => '8 (812) 924-57-92',
                 'hot_normalized' => '+78129245792' */
